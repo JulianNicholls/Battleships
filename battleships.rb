c25376eea3352ce0bf1bc38d7c35fbf3255e2c14
Julian Nicholls <juliannicholls29@gmail.com>
2014-06-05 19:39:01 +0100

    Moving towards the game.
    The computer's ships are now placed randomly.
    The user can repeatedly enter a location to attack.
    The hit/miss is shown, and if a computer ship is sunk, that too is shown.

--- battleships.rb
+++ battleships.rb
@@ -1,67 +1,66 @@
 #! /usr/bin/env ruby -I.
 
 require 'grid'
+require 'shiptypes'
 
 class Battleships
+  SHIPS = [AircraftCarrier, Battleship, Cruiser, 
+           Destroyer, Destroyer, Submarine, Submarine]
+           
   def initialize
-    @cgrid  = Grid.new
-    @mygrid = Grid.new( :visible )
+    @computer_grid  = Grid.new    # ( :visible )  # for now
+    @player_grid = Grid.new( :visible )
   end
 
   def show
-    puts "         Other"
-    puts @cgrid.to_s( :headers )
-
-    puts "\n         Player"
-    puts @mygrid.to_s( :headers )
+    lefts  = @computer_grid.to_s( :headers ).lines
+    rights = @player_grid.to_s( :headers ).lines
+    
+    puts "\n        Computer                      Player"
+    lefts.each_with_index do |left, idx|
+      puts "#{left.chomp}      #{rights[idx].chomp}"
+    end
   end
 
-  def fill
-    @mygrid.set( 'A1' )   ; @cgrid.set( 'A1' )
-    @mygrid.set( 'A2' )   ; @cgrid.set( 'A2' )
-    @mygrid.set( 'A3' )   ; @cgrid.set( 'A3' )
-    @mygrid.set( 'A4' )   ; @cgrid.set( 'A4' )
-    @mygrid.set( 'A5' )   ; @cgrid.set( 'A5' )
-
-    @mygrid.set( 'C3' )   ; @cgrid.set( 'C3' )
-    @mygrid.set( 'C4' )   ; @cgrid.set( 'C4' )
-    @mygrid.set( 'C5' )   ; @cgrid.set( 'C5' )
-    @mygrid.set( 'C6' )   ; @cgrid.set( 'C6' )
-
-    @mygrid.set( 'E3' )   ; @cgrid.set( 'E3' )
-    @mygrid.set( 'F3' )   ; @cgrid.set( 'F3' )
-    @mygrid.set( 'G3' )   ; @cgrid.set( 'G3' )
-
-    @mygrid.set( 'I3' )   ; @cgrid.set( 'I3' )
-    @mygrid.set( 'J3' )   ; @cgrid.set( 'J3' )
-
-    @mygrid.set( 'I7' )   ; @cgrid.set( 'I7' )
-    @mygrid.set( 'J7' )   ; @cgrid.set( 'J7' )
-
-    @mygrid.set( 'C8' )   ; @cgrid.set( 'C8' )
-    @mygrid.set( 'E10' )  ; @cgrid.set( 'E10' )
+  def fill_computer
+    @computer_ships = []
+    @computer_ships_sunk = []
+    
+    SHIPS.each { |ship| @computer_ships << ship.new( @computer_grid ) }
   end
   
-  def attack
-    @cgrid.attack( 'A1' ) 
-    @cgrid.attack( 'A2' ) 
-    @cgrid.attack( 'A3' ) 
-    @cgrid.attack( 'A4' ) 
-    @cgrid.attack( 'A5' ) 
-    @cgrid.attack( 'C3' ) 
-    @cgrid.attack( 'D4' ) 
-    @cgrid.attack( 'E3' ) 
-    @cgrid.attack( 'F3' ) 
-    @cgrid.attack( 'G5' ) 
-    @cgrid.attack( 'I6' ) 
-    @cgrid.attack( 'J6' ) 
-    @cgrid.attack( 'J7' )     
-    @cgrid.attack( 'C8' ) 
+  def fill_user # temporary
+    @player_ships = []
+    @player_ships_sunk = []
+    
+    SHIPS.each { |ship| @player_ships << ship.new( @player_grid ) }
+  end
+  
+  def player_play
+    print "\nLocation: "
+    loc = STDIN.gets.chomp
+    puts @computer_grid.attack( loc ) ? 'HIT!' : 'You Missed'
+    check_sunk
+  end
+  
+  def check_sunk
+    @computer_ships.each do |ship|
+      next unless ship.sunk?
+      
+      unless @computer_ships_sunk.include? ship
+        @computer_ships_sunk << ship
+        puts "You sunk a #{ship.name}"
+      end
+    end
   end
 end
  
 b = Battleships.new
 
-b.fill
-b.attack
-b.show
+b.fill_computer
+b.fill_user
+
+loop do
+  b.show
+  b.player_play
+end
