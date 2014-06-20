f93a045a9044b303c01d957ab95cff7f18e0f217
Julian Nicholls <juliannicholls29@gmail.com>
2014-06-20 19:11:23 +0100

    CPU Player thinking and attacking

---
+++ cpu_player.rb
@@ -0,0 +1,43 @@
+require 'gridpos.rb'
+
+module Battleships
+  # CPU Player. Initially just a random shot.
+  class CPUPlayer
+    def initialize( game )
+      @game = game
+    end
+
+    def update
+      update_attack   if @game.phase == :cpu_turn
+      update_thinking if @game.phase == :thinking
+    end
+
+    def set_thinking
+      @game.phase = :thinking
+      @think_time = Time.now + 0.5 + rand
+    end
+
+    private
+
+    def update_thinking
+      return if Time.now < @think_time
+
+      @game.phase = :cpu_turn
+    end
+
+    def update_attack
+      pos  = ''
+      grid = @game.player_grid
+
+      loop do
+        pos   = GridPos.random_pos
+        state = grid.cell_at( pos ).state
+
+        break unless [:miss, :hit].include? state
+      end
+
+      grid.attack pos
+      @game.phase = :player_turn
+    end
+  end
+end
